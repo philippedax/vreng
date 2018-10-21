00649e64982027eae7f799903b1ca637d271e28a
philippedax <philippe.dax@gmail.com>
2018-10-21 18:41:48 +0200

    move support/github .github

--- support/ubit_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-g++ -o ubit_test -g -O2 -Wall -fno-strict-aliasing -ffriend-injection -I/usr/include/freetype2 \
- -L/usr/X11R6/lib ubit_test.cpp -lubit -L/home/dan/dev/deliveries/ubit-99.99.99/lib \
- -L/usr/X11R6/lib -lXpm -L/usr/X11R6/lib  -lSM -lICE -L/usr/X11R6/lib -lXmu -lX11 -lfreetype -lm -lnsl -lpthread
-
-
