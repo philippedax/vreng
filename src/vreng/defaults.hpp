3c5184aa7526235f06e446edb82b1a003aab3d90
Philippe Dax <philippe.dax@gmail.com>
2021-06-12 11:18:41 +0200

    cleaning defaults

--- src/vreng/defaults.hpp
+++ src/vreng/defaults.hpp
@@ -1,8 +1,8 @@
 //---------------------------------------------------------------------------
-// VREng (Virtual Reality Engine)	http://vreng.enst.fr/
+// VREng (Virtual Reality Engine)	http://www.vreng.enst.fr/
 //
-// Copyright (C) 1997-2008 Philippe Dax
-// Telecom-ParisTech (Ecole Nationale Superieure des Telecommunications)
+// Copyright (C) 1997-2021 Philippe Dax
+// Telecom-Paris (Ecole Nationale Superieure des Telecommunications)
 //
 // VREng is a free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public Licence as published by
@@ -30,50 +30,57 @@
 #define DEF_VACS_SERVER		"localhost"
 #define DEF_VRUM_SERVER		"localhost"
 #define DEF_MYSQL_SERVER	"localhost"
-#else
+#else	// by default
+
 #define IPMC_ENABLED		1	// ipmulticast enabled
-#define DEF_HTTP_SERVER		"localhost"	// was "www.vreng.enst.fr" but recently unreachable
-						// because it is no more maintained today
-						// the httpd server and its datas associated
-						// should be well known at a stable location (url)
-						// and so reachable for everybody
-						// allowing multi-users behavior
-						// localhost is not the ideal solution (mono-user)
-						// localhost allows to run simply autonomously.
-						// see also DEF_URL_PFX witch can be changed
+#define DEF_HTTP_SERVER		"localhost"
+				// was "www.vreng.enst.fr" but recently unreachable
+				// because it is no more maintained today
+				// the httpd server and its datas associated
+				// should be well known at a stable location (url)
+				// and so reachable for everybody
+				// allowing multi-users behavior
+				// localhost is not the ideal solution (mono-user)
+				// localhost allows to run simply autonomously.
+				// see also DEF_URL_PFX witch can be changed
 #define DEF_VJS_SERVER		"localhost"
 #define DEF_VAPS_SERVER		"localhost"
 #define DEF_VACS_SERVER		"localhost"
 #define DEF_VRUM_SERVER		"localhost"
 #define DEF_MYSQL_SERVER	"localhost"
-#endif
+#endif	// def servers
 
-#define VRE_VERSION	8			// current version of vre files
+#define VRE_VERSION	8	// current stable version of vre files
 
 /* default URLs */
-#define GEN_URL(h, u, p) 	("http://" h u p)
+#define GEN_URL(h, u, p) 	("http://" h u p) // don't change this macro
 #if !defined(DEF_URL_PFX)
+// url prefix allowing access to the htdocs location
 // default url prefix: can be changed to be adapted to your site.
-#define DEF_URL_PFX	"~dax/vreng/"	// prefix to tune, should be "" (old prefix)
-					// it is the HTDOCS equivallent for your
-					// local httpd server (after http://httpserver/)
-					// for example :
-					// ~/public_html/vreng/ or 
-					// ~/Sites/vreng/ (Mac)
-					// where vreng/ is the same as htdocs/ directory
-					// of this distribution.
-#endif
-#define DEF_URL_WORLD	"/vre/v8/Rendezvous.vre"	// old location: /vre/v8/Rendezvous.vre
-#define DEF_URL_WORLD_BAK "/vre/v8/Rendezvous.vre" // old location: /vre/v8/Rendezvous.vre
-#define DEF_URL_FRONT	"/gif/default.gif"
-#define DEF_URL_BACK	"/gif/default.1.gif"
-#define DEF_URL_BALL	"/gif/ball.gif"
-#define DEF_URL_FONT	"/txf/arial-br-18.txf"
-#define DEF_URL_PAPER	"/jpg/paper.jpg"
-#define DEF_URL_NOISE	"/mp3/sheet.mp3"
-#define DEF_URL_JAR	"/jar/vrengapp.jar"
 
-/* default IPmc addresses & ports */
+#define DEF_URL_PFX	"~dax/vreng/"
+				// prefix to tune, should be "" (old prefix)
+				// it is the HTDOCS equivallent for your
+				// local httpd server (after http://httpserver/)
+				// for example :
+				//   ~/public_html/vreng/ or 
+				//   ~/Sites/vreng/ (Mac)
+				// where vreng/ is the same as htdocs/ directory
+				// in this distribution.
+#endif
+
+// these paths are relative to the htdocs directory
+#define DEF_URL_WORLD	"/vre/v8/Rendezvous.vre"   // location: /vre/v8/Rendezvous.vre
+#define DEF_URL_WORLD_BAK "/vre/v8/Rendezvous.vre" // alt location: /vre/Rendezvous.vre
+#define DEF_URL_FRONT	"/gif/default.gif"	// for user box
+#define DEF_URL_BACK	"/gif/default.1.gif"	// for user box
+#define DEF_URL_BALL	"/gif/ball.gif"		// ball by default
+#define DEF_URL_FONT	"/txf/arial-br-18.txf"	// txf font by default
+#define DEF_URL_PAPER	"/jpg/paper.jpg"	// paper for book
+#define DEF_URL_NOISE	"/mp3/sheet.mp3"	// noise for sheet
+#define DEF_URL_JAR	"/jar/vrengapp.jar"	// for vjs server
+
+/* default IPmulticast addresses & ports */
 #define VRENG_MADDR_BASE	"224.255.0.0"
 #define VRENG_MPORT_BASE	52656
 #define DEF_VRE_MPORT		52666
@@ -81,31 +88,29 @@
 #define DEF_VRE_MADDR		"224.255.0.1"
 #define DEF_VRE_CHANNEL		"224.255.0.1/52666/127"
 #define DEF_MANAGER_CHANNEL	"224.255.0.0/52656/127"
-
 #define VACS_PORT		4150 + VRE_VERSION	// port of vacs server
-#define SAP_ADDR		"224.2.127.254"
-#define SAP_PORT		9875
+#define SAP_ADDR		"224.2.127.254"		// address of Session Address Protocol
+#define SAP_PORT		9875			// port of Session Address Protocol
 
-#define DEF_RATE		50	// 50 frames/sec
-#define DEF_RATIO3D		(16./9.)// screen ratio 16/9
-#define DEF_WIDTH3D		960 	// screen width
-#define DEF_HEIGHT3D		540 	// screen height
-#define DEF_MAXSIMCON		8	// MAX simultaneous threaded connections
-#define DEF_MAXFRAMES		255	// MAX frames
-#define DEF_REFRESH_TIMEOUT	1.5	//orig: 1.5
+#define DEF_RATE		50		// 50 frames/sec
+#define DEF_RATIO3D		(16./9.)	// screen ratio 16/9
+#define DEF_WIDTH3D		960 		// screen width
+#define DEF_HEIGHT3D		540 		// screen height
+#define DEF_MAXSIMCON		8		// MAX simultaneous threaded connections
+#define DEF_MAXFRAMES		255		// MAX frames
+#define DEF_REFRESH_TIMEOUT	1.5		//orig: 1.5
 
-#define PATH_LEN		128	// max lenght of path file
-#define CHAN_LEN		32	// max length of a channel
-#define GROUP_LEN		16	// max length of a group
-#define URL_LEN			256	// max length of an url
-#define USER_LEN		9	// max lenght of user name
-#define OBJNAME_LEN		64	// max lenght of object name
-#define HNAME_LEN		11	// max length of understandable name
-#define ACTIONNAME_LEN		16	// max length of action name
-#define ACTIONSNUMBER		64	// number max of actions per object
-#define PROPSNUMBER		20	// number max of object's properties
-
-#define NEEDLOOPBACK		0	// no loopback for ipmulticast
+#define PATH_LEN		128		// max lenght of path file
+#define CHAN_LEN		32		// max length of a channel
+#define GROUP_LEN		16		// max length of a group
+#define URL_LEN			256		// max length of an url
+#define USER_LEN		9		// max lenght of user name
+#define OBJNAME_LEN		64		// max lenght of object name
+#define HNAME_LEN		11		// max length of understandable name
+#define ACTIONNAME_LEN		16		// max length of action name
+#define ACTIONSNUMBER		64		// number max of actions per object
+#define PROPSNUMBER		20		// number max of object's properties
+#define NEEDLOOPBACK		0		// no loopback for ipmulticast
 
 #ifndef TRUE
 #define TRUE 1
