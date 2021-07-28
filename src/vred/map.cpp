b315649f2ce3b6abd62b858cb86d0a4ad4c46919
Philippe Dax <philippe.dax@gmail.com>
2021-07-28 08:41:57 +0200

    cleaning up map.cpp in vred

--- src/vred/map.cpp
+++ src/vred/map.cpp
@@ -40,40 +40,40 @@ char* HttpGet(char* serverName, char* chemin, int& pos)
   
   memset((char *)&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
-  memcpy((char *)&serv_addr.sin_addr,(char *)hostptr->h_addr,hostptr->h_length);
+  memcpy((char *) &serv_addr.sin_addr, (char *)hostptr->h_addr, hostptr->h_length);
   serv_addr.sin_port = htons(80);
 
   int sockfd;
-  if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 #ifdef VERBOSE
     printf("socket error 1\n");
 #endif
     return 0;
   }
 
-  if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+  if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 #ifdef VERBOSE
     printf("socket error 2\n");
 #endif
     return 0;
   } 
 
-  int res=0;
-  int taille=512;
-  pos=0;
+  int res = 0;
+  int taille = 512;
+  pos = 0;
 
-  char* temp=(char*)malloc(taille);
+  char* temp = (char*) malloc(taille);
 
   sprintf(temp,"GET /%s HTTP/1.0\r\n\r\n", chemin);
-  write(sockfd,temp,strlen(temp));
+  write(sockfd, temp, strlen(temp));
 
 #ifdef VERBOSE
   printf("waiting for reply\n");
 #endif
 
   //Premier coup
-  res=read(sockfd,temp,taille);
-  if (res==-1) {
+  res = read(sockfd, temp, taille);
+  if (res == -1) {
 #ifdef VERBOSE
     printf("erreur !\n"); 
 #endif
@@ -83,10 +83,10 @@ char* HttpGet(char* serverName, char* chemin, int& pos)
   }  
 
   int marker;
-  for(marker=0;marker<taille-1;marker++) {
-    if (temp[marker]=='\n'&&temp[marker+1]=='\r') break;
+  for (marker=0; marker < taille-1; marker++) {
+    if (temp[marker] == '\n' && temp[marker+1] == '\r') break;
   }
-  if (marker==taille-1) {
+  if (marker == taille-1) {
 #ifdef VERBOSE
     printf("erreur !\n"); 
 #endif
@@ -95,18 +95,18 @@ char* HttpGet(char* serverName, char* chemin, int& pos)
     return 0;    
   }
 
-  marker+=3;
+  marker += 3;
 #ifdef VERBOSE
-  printf("Marker found=%d\n",marker);
+  printf("Marker found=%d\n", marker);
 #endif
 
-  for(int i=marker;i<res;i++) {
-    temp[i-marker]=temp[i];
+  for (int i=marker; i < res; i++) {
+    temp[i-marker] = temp[i];
   }
 
-  pos=res-marker;
+  pos = res-marker;
 
-  if (res==0) {
+  if (res == 0) {
     close(sockfd);
     return temp;
   }
@@ -114,17 +114,16 @@ char* HttpGet(char* serverName, char* chemin, int& pos)
   //Voila la suite
   while (1) {   
 
-    taille+=512;
-    temp=(char*)realloc(temp,taille);
-    res=read(sockfd,temp+pos,512);
-    pos+=res;
+    taille += 512;
+    temp = (char*) realloc(temp, taille);
+    res = read(sockfd, temp+pos, 512);
+    pos += res;
     
-    if (res==-1) break;
-    if (res==0) break;
-
+    if (res == -1) break;
+    if (res == 0) break;
   }
 
-  if (res==-1) {
+  if (res == -1) {
 #ifdef VERBOSE
     printf("erreur !\n"); 
 #endif
@@ -134,7 +133,7 @@ char* HttpGet(char* serverName, char* chemin, int& pos)
   }
 
 #ifdef VERBOSE
-  printf("Ok, fini, taille=%d\n",pos);
+  printf("Ok, fini, taille=%d\n", pos);
 #endif
 
   close(sockfd);
@@ -144,164 +143,164 @@ char* HttpGet(char* serverName, char* chemin, int& pos)
 const GLubyte* const Map::getData()
 {
   if (error) return 0;
-  if (data!=0) return data;
+  if (data != 0) return data;
 
   unsigned char rgbpalette[3*256];
   int tailleGif;
   char* fromWeb;
 
 #ifdef VERBOSE
-  printf("url '%s'\n",url);
+  printf("url '%s'\n", url);
 #endif 
 
   //parsing
 
-  if (strncmp(url,"file://",7)==0) {
+  if (strncmp(url, "file://", 7) == 0) {
 
 #ifdef VERBOSE
-    printf("File url %s\n",url);
+    printf("File url %s\n", url);
 #endif    
-    FILE* in=fopen(url+7,"rb");
-    if (in==NULL) {
+    FILE* in = fopen(url+7, "rb");
+    if (in == NULL) {
       printf("file not found !\n");
       free(url);
       url = 0;
-      error=true;
-      data=0;
+      error = true;
+      data = 0;
       return 0;
     }
-    fseek(in,0,SEEK_END);
+    fseek(in, 0, SEEK_END);
     tailleGif = ftell(in);
-    fseek(in,0,SEEK_SET);
+    fseek(in, 0, SEEK_SET);
 
 #ifdef VERBOSE
-    printf("filesize=%d\n",tailleGif);
+    printf("filesize=%d\n", tailleGif);
 #endif
                                     
-    fromWeb=(char *)malloc(tailleGif);
+    fromWeb = (char *) malloc(tailleGif);
                                             
-    if (fromWeb==NULL) {
+    if (fromWeb == NULL) {
       free(url);
       url = 0;
-      error=true;
-      data=0;
+      error = true;
+      data = 0;
       fclose(in);
       return 0;
     }
 
-    int taille=fread(fromWeb,1,tailleGif,in);
-    if (taille!=tailleGif) {
+    int taille = fread(fromWeb, 1, tailleGif, in);
+    if (taille != tailleGif) {
 	printf("Error while loading the file\n");
 	free(fromWeb);
 	fclose(in);
 	free(url);
 	url = 0;
-	error=true;
-	data=0;
+	error = true;
+	data = 0;
 	return 0;
     }
-
     fclose(in);
 
-  } else if (strncmp(url,"http://",7)==0) {
+  } else if (strncmp(url, "http://", 7) == 0) {
     int i;
-    for(i=7;url[i]!=0;i++)
-      if (url[i]=='/') 
+    for (i=7; url[i] != 0; i++) {
+      if (url[i] == '/') 
 	break;
-    
-    if (url[i]==0) {
-      printf("Mauvaise http url %s\n",url);
-      free(url); url = 0;
-      error=true;
-      data=0;
-      return 0;    
     }
     
-    url[i]=0;
+    if (url[i] == 0) {
+      printf("Mauvaise http url %s\n", url);
+      free(url); url = 0;
+      error = true;
+      data = 0;
+      return 0;    
+    }
+    url[i] = 0;
     
     //Get http
-    fromWeb=HttpGet(url+7,url+i+1,tailleGif);
-    if (fromWeb==0) {
+    fromWeb = HttpGet(url+7, url+i+1, tailleGif);
+    if (fromWeb == 0) {
       free(url); url = 0;
-      error=true;
-      data=0;
+      error = true;
+      data = 0;
       return 0;
     }
   } else if (url[0] == '/') {
     int i;
-    for(i=1;url[i]!=0;i++)
-      if (url[i]=='/') 
+    for (i=1; url[i]!=0; i++) {
+      if (url[i] == '/') 
 	break;
-    
-    if (url[i]==0) {
-      printf("Mauvaise http url %s\n",url);
-      free(url); url = 0;
-      error=true;
-      data=0;
-      return 0;    
     }
     
-    url[i]=0;
+    if (url[i] == 0) {
+      printf("Mauvaise http url %s\n", url);
+      free(url); url = 0;
+      error = true;
+      data = 0;
+      return 0;    
+    }
+    url[i] = 0;
     
     //Get http
-    fromWeb=HttpGet(url+1,url+i+1,tailleGif);
-    if (fromWeb==0) {
+    fromWeb = HttpGet(url+1, url+i+1, tailleGif);
+    if (fromWeb == 0) {
       free(url); url = 0;
-      error=true;
-      data=0;
+      error = true;
+      data = 0;
       return 0;
     }
   } else {
 
 #ifdef VERBOSE
-    printf("url non reconnue '%s'\n",url);
+    printf("url non reconnue '%s'\n", url);
 #endif
     free(url); url = 0;
-    error=true;
-    data=0;
+    error = true;
+    data = 0;
     return 0;
   }
   
   free(url); url = 0;
 
   //decompresse
-  int l,h;
+  int l, h;
 #ifdef VERBOSE
-  unsigned char* bob=load_gif((unsigned char*)fromWeb,tailleGif,rgbpalette,l,h,1);
+  unsigned char* bob = load_gif((unsigned char*)fromWeb,tailleGif, rgbpalette, l, h, 1);
   printf("taille x=%d, y=%d\n", l, h);
 #else
-  unsigned char* bob=load_gif((unsigned char*)fromWeb,tailleGif,rgbpalette,l,h,0);
+  unsigned char* bob = load_gif((unsigned char*)fromWeb, tailleGif, rgbpalette, l, h, 0);
 #endif
-  length=l;
-  height=h;
-  if (bob==0) {
+  length = l;
+  height = h;
+  if (bob == 0) {
     printf("Gif: error\n"); 
-    error=true;
-    data=0;
+    error = true;
+    data = 0;
     return 0;
   }
 
-  if (data!=0) free(data);
-  data=(GLubyte*)malloc(length*height*3);
-  if (data==0) { 
+  if (data != 0) free(data);
+  data = (GLubyte*) malloc(length*height*3);
+  if (data == 0) { 
     free(bob);
 #ifdef VERBOSE
     printf("Gif: error, not enough memory\n");
 #endif
-    error=true;
-    data=0;
+    error = true;
+    data = 0;
     return 0;
   }
 
   //unpaletize
-  for(int j=0;j<height;j++)
-    for(int i=0;i<length;i++) {
+  for (int j=0; j< height; j++) {
+    for(int i=0; i< length;i++) {
 
-      int car=int(bob[j*length+i]);
+      int car = int(bob[j*length+i]);
       data[3*(j*length+i)]   = rgbpalette[car*3];
       data[3*(j*length+i)+1] = rgbpalette[car*3+1];
       data[3*(j*length+i)+2] = rgbpalette[car*3+2];
     }
+  }
 
   free(bob);
 
@@ -315,7 +314,7 @@ const GLubyte* const Map::getData()
   data = (GLubyte*) finaltex;
 
 #ifdef VERBOSE
-  printf("Gif: OK %d %d\n",length,height);
+  printf("Gif: OK %d %d\n", length, height);
 #endif
 
   return data;
