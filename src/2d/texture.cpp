cbf440103dfd111d0c2513b254de61b050d177a8
Philippe Dax <philippe.dax@gmail.com>
2021-11-16 15:39:06 +0100

    fixed delete img when textures are updated

--- src/2d/texture.cpp
+++ src/2d/texture.cpp
@@ -107,7 +107,7 @@ void Texture::update()
         }
       }
       // once the texture resized we can delete its container img
-#if 0 //dax
+#if 1 //dax
       if ((*it)->loaded) {
         if ((*it)->img) delete (*it)->img; // opengl has its own copy of pixels
         (*it)->img = NULL;
