de03e0c3bd380ad23a14c543f725ae2490507994
Philippe Dax <philippe.dax@gmail.com>
2023-01-22 17:05:36 +0100

    cleanup dumptable oclass.cpp

--- src/wo/oclass.cpp
+++ src/wo/oclass.cpp
@@ -76,7 +76,7 @@ const OClass * OClass::getOClass(const char *type_name)
     }
   }
   error("type_name=%s not found, please upgrade VREng!", type_name);
-  //dumpTable();
+  dumpTable();
   return (OClass *) NULL;
 }
 
