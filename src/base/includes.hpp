47a46a488a4e2c9f637decdafde38d27266640f0
Philippe Dax <philippe.dax@gmail.com>
2023-09-29 09:20:13 +0200

    cleanup includes.hpp

--- src/base/includes.hpp
+++ src/base/includes.hpp
@@ -18,8 +18,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //---------------------------------------------------------------------------
-#ifndef SYSDEP_HPP
-#define SYSDEP_HPP
+#ifndef INCLUDES_HPP
+#define INCLUDES_HPP
 
 
 #if HAVE_CONFIG_H
