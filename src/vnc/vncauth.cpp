cc7d7c2087ada807ed32c70e52addeef0d3bc839
Philippe Dax <philippe.dax@gmail.com>
2024-03-31 15:37:00 +0200

    cleanup vnc/vncauth.cpp

--- src/vnc/vncauth.cpp
+++ src/vnc/vncauth.cpp
@@ -2,7 +2,7 @@
 // VREng (Virtual Reality Engine)	http://vreng.enst.fr/
 //
 // Copyright (C) 1997-2008 Philippe Dax
-// Telecom-ParisTech (Ecole Nationale Superieure des Telecommunications)
+// Telecom-Paris (Ecole Nationale Superieure des Telecommunications)
 //
 // VREng is a free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public Licence as published by
@@ -33,25 +33,24 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
- *  USA.
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-/*
+/**
  * vncauth.cpp - Functions for VNC password management and authentication.
  */
 #include "vreng.hpp"
-#include "file.hpp"	// open, close
 #include "vncauth.hpp"
+#include "file.hpp"	// open, close
 
 
-/*
+/**
  * We use a fixed key to store passwords, since we assume that our local
  * file system is secure but nonetheless don't want to store passwords
  * as plaintext.
  */
 uint8_t fixedkey[8] = {23,82,107,6,35,78,88,7};
 
-/*
+/**
  * Encrypt a password and store it in a file.  Returns 0 if successful,
  * 1 if the file could not be written.
  */
@@ -66,23 +65,24 @@ int vncEncryptAndStorePasswd(char *passwd, char *fname)
   chmod(fname, S_IRUSR|S_IWUSR);
 
   /* pad password with nulls */
-  for (uint32_t i=0; i < 8; i++)
+  for (uint32_t i=0; i < 8; i++) {
     encryptedPasswd[i] = (i < strlen(passwd)) ? passwd[i] : '\0';
+  }
 
-  /* Do encryption in-place - this way we overwrite our copy of the plaintext
-     password */
+  // Do encryption in-place - this way we overwrite our copy of the plaintext password
   deskey(fixedkey, EN0);
 
   des(encryptedPasswd, encryptedPasswd);
 
-  for (int i=0; i < 8; i++)
+  for (int i=0; i < 8; i++) {
     putc(encryptedPasswd[i], fp);
+  }
   file->close();
   delete file;
   return 0;
 }
 
-/*
+/**
  * Decrypt a password from a file.  Returns a pointer to a newly allocated
  * string containing the password or a null pointer if the password could
  * not be retrieved for some reason.
@@ -113,18 +113,20 @@ char * vncDecryptPasswdFromFile(char *fname)
   return reinterpret_cast<char *>(passwd);
 }
 
-/*
+/**
  * Encrypt CHALLENGESIZE bytes in memory using a password.
  */
 void vncEncryptBytes(uint8_t *bytes, char *passwd)
 {
   uint8_t key[8];
 
-  /* key is simply password padded with nulls */
-  for (uint32_t i=0; i < 8; i++)
+  // key is simply password padded with nulls
+  for (uint32_t i=0; i < 8; i++) {
     key[i] = (i < strlen(passwd)) ? passwd[i] : '\0';
+  }
   deskey(key, EN0);
 
-  for (int i=0; i < CHALLENGESIZE; i += 8)
+  for (int i=0; i < CHALLENGESIZE; i += 8) {
     des(bytes+i, bytes+i);
+  }
 }
