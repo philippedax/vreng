710447ab263acfad95bfb292b9fdf9be0adf53f5
Philippe Dax <philippe.dax@gmail.com>
2024-04-08 16:59:31 +0200

    cleanup notused uobject.hpp

--- src/ubit/uobject.hpp
+++ src/ubit/uobject.hpp
@@ -116,10 +116,9 @@ namespace ubit {
   public:
     struct MetaClass : public UClass {
       MetaClass() : UClass("UObject") {}
-      //virtual bool isInstance(UObject& obj) const { return dynamic_cast<UObject*>(&obj); }
-      //dax virtual bool isInstance(UObject* obj) const { return dynamic_cast<UObject*>(&obj); }
-      virtual UObject* newInstance() const {return null;} 
-      virtual UStyle* newStyle() const {return null;}
+      //dax virtual bool isInstance(UObject& obj) const { return dynamic_cast<UObject*>(&obj); }
+      //virtual UObject* newInstance() const {return null;} 
+      //virtual UStyle* newStyle() const {return null;}
     };
     
     UObject();
