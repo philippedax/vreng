6f6d27bfbdf50b277daae486de8d764e7f35ef36
Philippe Dax <philippe.dax@gmail.com>
2024-04-10 17:43:34 +0200

    cleanup theme.*pp

--- src/gui/theme.hpp
+++ src/gui/theme.hpp
@@ -1,8 +1,8 @@
 //---------------------------------------------------------------------------
-// VREng (Virtual Reality Engine)	http://vreng.enst.fr/
+// VREng (Virtual Reality Engine)	https://github.com/philippedax/vreng
 //
 // Copyright (C) 1997-2009 Philippe Dax
-// Telecom-ParisTech (Ecole Nationale Superieure des Telecommunications)
+// Telecom-Paris (Ecole Nationale Superieure des Telecommunications)
 //
 // VREng is a free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public Licence as published by
@@ -28,8 +28,7 @@
  * Theme class
  */
 class Theme {
-public:
-
+ public:
   Theme();
   
   void init();
