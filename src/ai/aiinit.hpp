8a499d81bcaf3f0c155dbf1a97fda11f506abac5
Philippe Dax <philippe.dax@gmail.com>
2024-04-18 12:20:52 +0200

    cleanup heading ai/*pp

--- src/ai/aiinit.hpp
+++ src/ai/aiinit.hpp
@@ -18,6 +18,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //---------------------------------------------------------------------------
+// aiinit.hpp
+//---------------------------------------------------------------------------
 #ifndef AIINIT_HPP
 #define AIINIT_HPP
 
