c47121cb19dacea40eba044f52103a509acc692f
Philippe Dax <philippe.dax@gmail.com>
2024-05-15 09:49:52 +0200

    add empty fap.cpp

---
+++ src/model/fap.cpp
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+// VREng (Virtual Reality Engine)	https://github.com/philippedax/vreng
+//
+// Copyright (C) 1997-2011 Philippe Dax
+// Telecom-Paris (Ecole Nationale Superieure des Telecommunications)
+//
+// VREng is a free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public Licence as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// VREng is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//---------------------------------------------------------------------------
+// fap.cpp
+//
+// Fap handling
+//---------------------------------------------------------------------------
+#include "vreng.hpp"
+#include "v3d.hpp"
+#include "humanoid.hpp"
+#include "fap.hpp"
+#include "body.hpp"	// body
+#include "http.hpp"	// httpOpen
+#include "cache.hpp"	// setCachePath, open
+#include "bone.hpp"	// V3d
+
+
