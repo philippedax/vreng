be2dd6d0bbb6dcbfc977dadef2da9b08fafadd1a
Philippe Dax <philippe.dax@gmail.com>
2024-10-29 09:54:16 +0100

    cleanup vrum/vrum.py

--- src/servers/vrum/vrum.py
+++ src/servers/vrum/vrum.py
@@ -6,9 +6,6 @@
 #
 # Samuel Tardieu - Mar 2003
 #
-
-MTU = 1500
-
 verbose = 1
 uport_offset = 1000
 maddr_base = '224.255.0.0'
@@ -16,9 +13,10 @@ mport_base = 52656
 mport_incr = 10
 ttl = 127
 timeout = 60
-
+MTU = 1500
 
 import multicast, select, socket, thread, time
+
 
 def launch (world, proto, maddr, mport, uport = None):
     if not uport: uport = mport - uport_offset
@@ -40,6 +38,7 @@ def launch (world, proto, maddr, mport, uport = None):
             pkt = ms.recv (MTU)
             for i in subscribers.keys():
                 us.sendto (pkt, i)
+
         if us in ready:
             # if verbose: print "%s(%s): unicast packet" % (world, proto)
             pkt, sender = us.recvfrom (MTU)
@@ -55,60 +54,60 @@ def launch (world, proto, maddr, mport, uport = None):
         if verbose: print "%s(%s): %d unicast subscribers" % \
            (world, proto, len (subscribers.keys()))
 
-def launchrtp (world, maddr, mport, uport = None):
+def rtp (world, maddr, mport, uport = None):
     thread.start_new_thread (launch, (world, "rtp", maddr, mport, uport))
     thread.start_new_thread (launch, (world, "rtcp", maddr, mport+1, uport))
 
-def launchudp (world, maddr, mport, uport = None):
+def udp (world, maddr, mport, uport = None):
     thread.start_new_thread (launch, (world, "udp", maddr, mport, uport))
 
 # Launch repeaters
 
-launchrtp ("Manager",      '224.255.0.0',  52656)
-launchrtp ("Rendezvous",   '224.255.0.1',  52666)
-launchrtp ("students",     '224.255.0.2',  52676)
-launchrtp ("eric",         '224.255.0.3',  52686)
-launchrtp ("infres",       '224.255.0.4',  52696)
-launchrtp ("checker",      '224.255.0.5',  52706)
-launchrtp ("tarots",       '224.255.0.6',  52716)
-launchrtp ("telecomparis", '224.255.0.7',  52726)
-launchrtp ("stadium",      '224.255.0.8',  52736)
-launchrtp ("pyramid",      '224.255.0.9',  52746)
-launchrtp ("maze",         '224.255.0.10', 52756)
-launchrtp ("dax",          '224.255.0.11', 52766)
-launchrtp ("danzart",      '224.255.0.12', 52776)
-launchrtp ("felon",        '224.255.0.13', 52786)
-launchrtp ("mouret",       '224.255.0.14', 52796)
-launchrtp ("mythology",    '224.255.0.15', 52806)
-launchrtp ("classroom",    '224.255.0.16', 52816)
-launchrtp ("house",        '224.255.0.17', 52826)
-launchrtp ("labyrinthe",   '224.255.0.18', 52836)
-launchrtp ("temple",       '224.255.0.19', 52846)
-launchrtp ("cubes",        '224.255.0.20', 52856)
-launchrtp ("forest",       '224.255.0.21', 52866)
-launchrtp ("tests",        '224.255.0.22', 52876)
-launchrtp ("beach",        '224.255.0.23', 52886)
-launchrtp ("movies",       '224.255.0.24', 52896)
-launchrtp ("meeting",      '224.255.0.25', 52906)
-launchrtp ("desktops",     '224.255.0.26', 52916)
-launchrtp ("entree",       '224.255.0.27', 52926)
-launchrtp ("fan",          '224.255.0.28', 52936)
-launchrtp ("ring",         '224.255.0.29', 52946)
-launchrtp ("board",        '224.255.0.30', 52956)
-launchrtp ("guide",        '224.255.0.31', 52966)
-launchrtp ("romain",       '224.255.0.32', 52976)
-launchrtp ("land",         '224.255.0.33', 52986)
-launchrtp ("factory",      '224.255.0.34', 52996)
-launchrtp ("forest2",      '224.255.0.35', 53006)
-launchrtp ("enstnet",      '224.255.0.36', 53016)
-launchrtp ("mondetest",    '224.255.0.37', 53026)
-launchrtp ("campus",       '224.255.0.38', 53036)
+rtp ("Manager",      '224.255.0.0',  52656)
+rtp ("Rendezvous",   '224.255.0.1',  52666)
+rtp ("students",     '224.255.0.2',  52676)
+rtp ("eric",         '224.255.0.3',  52686)
+rtp ("infres",       '224.255.0.4',  52696)
+rtp ("checker",      '224.255.0.5',  52706)
+rtp ("tarots",       '224.255.0.6',  52716)
+rtp ("telecomparis", '224.255.0.7',  52726)
+rtp ("stadium",      '224.255.0.8',  52736)
+rtp ("pyramid",      '224.255.0.9',  52746)
+rtp ("maze",         '224.255.0.10', 52756)
+rtp ("dax",          '224.255.0.11', 52766)
+rtp ("danzart",      '224.255.0.12', 52776)
+rtp ("felon",        '224.255.0.13', 52786)
+rtp ("mouret",       '224.255.0.14', 52796)
+rtp ("mythology",    '224.255.0.15', 52806)
+rtp ("classroom",    '224.255.0.16', 52816)
+rtp ("house",        '224.255.0.17', 52826)
+rtp ("labyrinthe",   '224.255.0.18', 52836)
+rtp ("temple",       '224.255.0.19', 52846)
+rtp ("cubes",        '224.255.0.20', 52856)
+rtp ("forest",       '224.255.0.21', 52866)
+rtp ("tests",        '224.255.0.22', 52876)
+rtp ("beach",        '224.255.0.23', 52886)
+rtp ("movies",       '224.255.0.24', 52896)
+rtp ("meeting",      '224.255.0.25', 52906)
+rtp ("desktops",     '224.255.0.26', 52916)
+rtp ("entree",       '224.255.0.27', 52926)
+rtp ("fan",          '224.255.0.28', 52936)
+rtp ("ring",         '224.255.0.29', 52946)
+rtp ("board",        '224.255.0.30', 52956)
+rtp ("guide",        '224.255.0.31', 52966)
+rtp ("romain",       '224.255.0.32', 52976)
+rtp ("land",         '224.255.0.33', 52986)
+rtp ("factory",      '224.255.0.34', 52996)
+rtp ("forest2",      '224.255.0.35', 53006)
+rtp ("enstnet",      '224.255.0.36', 53016)
+rtp ("mondetest",    '224.255.0.37', 53026)
+rtp ("campus",       '224.255.0.38', 53036)
 
-launchudp ("android-man",  '224.255.0.25', 52918)
-launchudp ("android-girl", '224.255.0.25', 52920)
-launchudp ("android-kang", '224.255.0.25', 52922)
-launchudp ("android-dog",  '224.255.0.25', 52924)
-launchudp ("android-wolf", '224.255.0.25', 52926)
+udp ("android-man",  '224.255.0.25', 52918)
+udp ("android-girl", '224.255.0.25', 52920)
+udp ("android-kang", '224.255.0.25', 52922)
+udp ("android-dog",  '224.255.0.25', 52924)
+udp ("android-wolf", '224.255.0.25', 52926)
 
 # Wait forever
 
