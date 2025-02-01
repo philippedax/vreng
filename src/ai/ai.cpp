f8b530f26b7a5c26118457fa058fcb3f82ddc257
Philippe Dax <philippe.dax@gmail.com>
2025-02-01 09:59:02 +0100

    cleanup renaming ai.cpp

--- src/ai/ai.cpp
+++ src/ai/ai.cpp
@@ -24,7 +24,7 @@
 //---------------------------------------------------------------------------
 #include "vreng.hpp"
 #include "ai.hpp"
-#include "object.hpp"
+#include "object.hpp"	// Object
 #include "gui.hpp"	// getClicked
 #include "world.hpp"	// current()
 #include "cache.hpp"	// setCachePath
@@ -52,28 +52,27 @@ void initOcaml()
 /** Fonctions de transformations de requetes */
 value recherche_Typegen(value _type, value _action)
 {
-  char *type = const_cast<char *>(String_val(_type));
-  char *action = const_cast<char *>(String_val(_action));
+  char *type = const_cast<char *> (String_val(_type));
+  char *action = const_cast<char *> (String_val(_action));
 
-  uint8_t oclick = 0;
-  float oclicked[7];
+  uint8_t clicks = 0;
+  float clicked[7];
 
   char *foundpos = new char[100];
   sprintf(foundpos, "N/A");
-  int nbelem = 0;
-  Object** objects = g.render.getDrawedObjects(&nbelem);
-  //echo("Here %s >> %s >> %d", objects[1]->typeName(), type, nbelem);
+  int nbobj = 0;
+  Object** objects = g.render.getDrawedObjects(&nbobj);
+  //echo("Here %s >> %s >> %d", objects[1]->typeName(), type, nbobj);
 
   int found = 0;
-  int foundelem = 0;
+  int foundobj = 0;
 
-  for (int i=1; i < nbelem; i++) {
+  for (int i=1; i < nbobj; i++) {
     echo("Here %d %s", i, objects[i]->typeName());
-
     if (! strcasecmp(objects[i]->typeName(), type) && objects[i]->isVisible()) {
-      //echo("found a type !");
+      //echo("found a type!");
       if (found == 0) {
-	foundelem = i;
+	foundobj = i;
 	found = 1;
       } else {
 	found = -1;
@@ -81,15 +80,14 @@ value recherche_Typegen(value _type, value _action)
       }
     }
   }
-
   if (found == 1) {
-    echo("Here: I execute the action on the object");
-    objects[foundelem]->runAction(action);
+    echo("Here: execute the action on the object");
+    objects[foundobj]->runAction(action);
   }
   if (found == -1 || found == 0) {
-    g.gui.getClicked(&oclick, oclicked);
-    if (oclicked[0] != MAXFLOAT && oclick == 0) {
-      //echo("I analyze your selection on %s", (g.gui.getSelectedObject())->name); 
+    g.gui.getClicked(&clicks, clicked);
+    if (clicked[0] != MAXFLOAT && clicks == 0) {
+      //echo("analyze your selection on %s", (g.gui.getSelectedObject())->name); 
       g.gui.getSelectedObject()->runAction(action);
       g.gui.initClicked();
       return Val_int(0);
@@ -99,10 +97,9 @@ value recherche_Typegen(value _type, value _action)
     echo("there are several elements of this type, please show");
     return Val_int(1);
   } else if (found == 0) {
-    echo("I don't see anything, please show");
+    echo("don't see anything, please show");
     return Val_int(1);
   }
-
   // demande designation.
   return Val_int(0);
 }
@@ -110,18 +107,18 @@ value recherche_Typegen(value _type, value _action)
 value recherche_Type(value _type)
 {
   value ret;
-  char *type = const_cast<char *>(String_val(_type));
-  uint8_t oclick = 0;
-  float oclicked[7];
+  char *type = const_cast<char *> (String_val(_type));
+  uint8_t clicks = 0;
+  float clicked[7];
 
   char *foundpos = new char[100];
   sprintf(foundpos, "N/A");
 
-  int nbelem = 0;
-  Object** objects = g.render.getDrawedObjects(&nbelem);
+  int nbobj = 0;
+  Object** objects = g.render.getDrawedObjects(&nbobj);
 
   int found = 0;
-  for (int i=0; i < nbelem; i++) {
+  for (int i=0; i < nbobj; i++) {
     if (! strcasecmp(objects[i]->typeName(), type) && objects[i]->isVisible()) {
       if (found) {
 	found = 0;
@@ -133,23 +130,17 @@ value recherche_Type(value _type)
   }
   if (found == 0) {
     //echo("found a type by click");
-    g.gui.getClicked(&oclick, oclicked);
-    if (oclicked[0] != MAXFLOAT && oclick == 0) {
+    g.gui.getClicked(&clicks, clicked);
+    if (clicked[0] != MAXFLOAT && clicks == 0) {
       sprintf(foundpos,"%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f",
-	      oclicked[0],
-	      oclicked[1],
-	      oclicked[2],
-	      oclicked[3],
-	      oclicked[4],
-	      oclicked[5],
-	      oclicked[6]);
+	      clicked[0], clicked[1], clicked[2], clicked[3], clicked[4], clicked[5], clicked[6]);
       g.gui.initClicked();
 
-      /* enlever les highlights des objets de meme type */
+      // enlever les highlights des objets de meme type
       g.render.highlight(type, false);
     }
     else {
-      /* mettre en highlight les objets de meme type */
+      // mettre en highlight les objets de meme type
       g.render.highlight(type, true);
     }
   }
@@ -171,12 +162,11 @@ value recherche_Type(value _type)
 
 value recherche_Objet(value mot)
 {
-  char *foundpos = new char[100];
   value ret;
+  char *foundpos = new char[100];
   sprintf(foundpos, "N/A");
-
 #if HAVE_LIBXML2
-  char *val = const_cast<char *>(String_val(mot));
+  char *val = const_cast<char *> (String_val(mot));
   char filename[64];
   Cache::setCachePath(World::current()->url, filename);
   Xml::selectXpathExpr(filename, "//*/@name", val, foundpos);
@@ -190,11 +180,11 @@ value recherche_Objet(value mot)
 /** fontions de deplacement de l'utilisateur */
 value deplacement_to_Objet(value px, value py, value pz, value ori, value depl)
 {
-  float posx = static_cast<float>(Double_val(px));
-  float posy = static_cast<float>(Double_val(py));
-  float posz = static_cast<float>(Double_val(pz));
-  float orient = static_cast<float>(Double_val(ori));
-  int deplacement = static_cast<int>(Int_val(depl));
+  float posx = static_cast<float> (Double_val(px));
+  float posy = static_cast<float> (Double_val(py));
+  float posz = static_cast<float> (Double_val(pz));
+  float orient = static_cast<float> (Double_val(ori));
+  int deplacement = static_cast<int> (Int_val(depl));
 
   // Deplacement
   if (! deplacement) {
@@ -211,56 +201,56 @@ value deplacement_to_Objet(value px, value py, value pz, value ori, value depl)
 
 value recherche_Func(value mot, value act)
 {
-  char *val = const_cast<char *>(String_val(mot));
-  char *action = const_cast<char *>(String_val(act));
-  int *listNumType = Vicinity::getTypeFromAction(val);
-  int nbtype = listNumType[0];
+  char *val = const_cast<char *> (String_val(mot));
+  char *action = const_cast<char *> (String_val(act));
+  int *listtype = Vicinity::getTypeFromAction(val);
+  int nbtype = listtype[0];
 
   //echo("I'am here %d %s %s !", nbtype, val, action);
 
   if (nbtype > 0) {
-    char** typelist = new char*[nbtype];
+    char** types = new char*[nbtype];
 
     for (int k=1 ; k < 1+nbtype ; k++) {
-      typelist[k-1] = Vicinity::translateNum2Type(listNumType[k]);
+      types[k-1] = Vicinity::translateNum2Type(listtype[k]);
     }
 
-    int nb = 0;
-    Object **objects = g.render.getVisibleObjects(typelist, nbtype, &nb);
+    int nbobj = 0;
+    Object **objects = g.render.getVisibleObjects(types, nbtype, &nbobj);
 
-    if (nb > 0 && strcasecmp(action, "NA")) {
+    if (nbobj > 0 && strcasecmp(action, "NA")) {
       //on lance la methode sur les objets visibles! faut-il la lancer sur tout?
-      for (int l=0; l<nb; l++) {
+      for (int l=0; l<nbobj; l++) {
 	if (! strcasecmp(objects[l]->typeName(), action))
 	  objects[l]->runAction(val);
       }
     }
     else {
-      if (nb == 1 && objects[0]) {
+      if (nbobj == 1 && objects[0]) {
         echo("object found, do \"%s\" action !", val);
         objects[0]->runAction(val);
       }
-      else if (nb > 1) {
-        echo("ambiguity, %d objects found, be more precise", nb);
+      else if (nbobj > 1) {
+        echo("ambiguity, %d objects found, be more precise", nbobj);
       }
       else {
         echo("no visible object for \"%s\", scan around the user", val);
         Vicinity *vicin = new Vicinity();
-        int nbproximity = 0;
-        Object* nearObj = vicin->searchProximityObject(typelist, nbtype);
+        int nbprox = 0;
+        Object* nearObj = vicin->searchProximityObject(types, nbtype);
         if (nearObj) {
 	  nearObj->runAction(val);
 	  echo("call the method on the nearest object");	
         }
-        else if (nbproximity > 1) {
-	  echo("test proxi %d", nbproximity);
+        else if (nbprox > 1) {
+	  echo("test proxi %d", nbprox);
         }
         else {
 	  echo("no action \"%s\" possible", val);
         }
       }
     }
-    if (typelist) delete[] typelist; typelist = NULL;
+    if (types) delete[] types; types = NULL;
     if (objects) free(objects);
   }
   return Val_int(0);
@@ -273,9 +263,8 @@ value deplacement_to_Proximite(value mot, value pos)
   float posy = localuser->pos.y;
   float posz = localuser->pos.z;
   float orient = localuser->pos.az;
-
 #if HAVE_LIBXML2
-  char *val = const_cast<char *>(String_val(mot));
+  char *val = const_cast<char *> (String_val(mot));
   char filename[64];
   Cache::setCachePath(World::current()->url, filename);
   res = Xml::selectProximity(filename, val, &posx, &posy, &posz, &orient);
@@ -287,27 +276,27 @@ value deplacement_to_Proximite(value mot, value pos)
   return Val_int(0);
 }
 
-/** fonctions d'affichage */
+/** Fonctions d'affichage */
 void msg_info(value mot)
 {
-  char *val = const_cast<char *>(String_val(mot));
-  echo("msgInfo::%s", val);
+  char *val = const_cast<char *> (String_val(mot));
+  echo("msginfo::%s", val);
 }
 
 void msg_debug(value mot)
 {
-  char *val = const_cast<char *>(String_val(mot));
+  char *val = const_cast<char *> (String_val(mot));
   echo("msg_debug::%s", val);
 }
 
 void viewed_objects(value mot)
 {
-  char *val = const_cast<char *>(String_val(mot));
+  char *val = const_cast<char *> (String_val(mot));
   Vicinity* vicin = new Vicinity(val);
   vicin->analScene();
 }
 
-/** fonctions de lecture des requetes */
+/** Fonctions de lecture des requetes */
 int read_request(const char *requete)
 {
   if (requete && (requete[0] == '@')) {
@@ -315,7 +304,7 @@ int read_request(const char *requete)
   }
   if (! strcmp(requete, "TEST")) {
     printf("TESTONS\n");
-    g.render.calculateFov(0,0,0,0, const_cast<char *>("TEST.jpg"));
+    g.render.calculateFov(0,0,0,0, const_cast<char *> ("TEST.jpg"));
     return 0;
   }
   if (! strcmp(requete, "C")) {
@@ -324,7 +313,7 @@ int read_request(const char *requete)
     return 0;
   }
   if (! strcmp(requete, "V")) {
-    Vicinity::show("I look at vicinity");
+    Vicinity::show("look at vicinity");
     return 0;
   }
   if (! strcmp(requete, "L")) {
