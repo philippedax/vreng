434356cc25f141fe0a3d617863fa87f313d5d96a
Philippe Dax <philippe.dax@gmail.com>
2025-02-19 10:16:11 +0100

    add conf/cache.sh sript

---
+++ conf/cache.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#
+# cache.sh
+#
+# Sets in cache some important files
+#
+# Philippe Dax - feb 2025
+#
+data=../data
+cache=$HOME/.vreng/cache
+
+p=$(basename $0)
+
+if [ ! -d $data ]; then
+  echo "$p: $data not found"
+  exit 1
+fi
+if [ ! -d $cache ]; then
+  echo "$p: $cache not found"
+  exit 1
+fi
+
+cp $data/vre/* $cache
+cp $data/gif/marble.gif $cache
+cp $data/gif/meeting.gif $cache
+cp $data/gif/mythology.gif $cache
+cp $data/gif/tarots.gif $cache
+cp $data/gif/beach.gif $cache
+cp $data/gif/dax.gif $cache
