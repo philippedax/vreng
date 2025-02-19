49835aa1350cd969a0c386223e0a8362f192e91c
Philippe Dax <philippe.dax@gmail.com>
2025-02-19 10:28:04 +0100

    update conf/cache.sh sript

--- conf/cache.sh
+++ conf/cache.sh
@@ -22,8 +22,9 @@ fi
 
 cp $data/vre/* $cache
 cp $data/gif/marble.gif $cache
-cp $data/gif/meeting.gif $cache
 cp $data/gif/mythology.gif $cache
 cp $data/gif/tarots.gif $cache
 cp $data/gif/beach.gif $cache
 cp $data/gif/dax.gif $cache
+cp $data/jpg/meeting.jpg $cache
+cp $data/txf/courier-br-46.txf $cache
