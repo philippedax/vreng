7976cfddf43ababffe2bc75287c65555b42aa3be
Philippe Dax <philippe.dax@gmail.com>
2025-02-22 11:43:42 +0100

    fix bad token step.cpp

--- src/world/step.cpp
+++ src/world/step.cpp
@@ -84,6 +84,11 @@ void Step::parser(char *l)
     else if (! stringcmp(l, "height")) l = parseFloat(l, &height, "height");
     else if (! stringcmp(l, "length")) l = parseFloat(l, &length, "length");
     else if (! stringcmp(l, "speed"))  l = parseFloat(l, &speed, "speed");
+    else {
+      error("token %s unrecognized", l);
+      l = parse()->nextToken();
+      break;
+    }
   }
   end_while_parse(l);
 
